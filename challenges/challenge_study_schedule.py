be7245fc120a3fad35d72a0d3045a547cfd25d41
Antonio Sabino <aslimajr@outlook.com>
2022-10-31 18:02:56 -0300

    Ajuste lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,8 +3,12 @@ def study_schedule(permanence_period, target_time):
         return None
     counter = 0
     for time_in, time_out in permanence_period:
-        if (type(time_in) is str or time_in is None or time_out is None or type(time_out) is str):
+        if (
+            type(time_in) is str or time_in is None or 
+            time_out is None or type(time_out) is str
+        ):
             return None
         if time_in <= target_time <= time_out:
             counter += 1
     return counter
+    

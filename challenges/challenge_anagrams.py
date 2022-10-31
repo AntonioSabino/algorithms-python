cbf11a2d50312219fe66fee80793ef5ef80e3c71
Antonio Sabino <aslimajr@outlook.com>
2022-10-31 18:22:37 -0300

    Resolução requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    if len(first_string) != len(second_string):
+        return False
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+    for letter in first_string:
+        if letter not in second_string:
+            return False
+        second_string = second_string.replace(letter, "", 1)
+    return True

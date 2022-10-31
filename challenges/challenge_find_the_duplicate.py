2a4133443db795f06a29693c534da96161daa7bf
Antonio Sabino <aslimajr@outlook.com>
2022-10-31 19:12:20 -0300

    Resolução requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    if len(nums) == 0 or len(nums) == 1:
+        return False
+    for num in nums:
+        if type(num) is not int or num < 0:
+            return False
+
+    nums.sort()
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+    return False

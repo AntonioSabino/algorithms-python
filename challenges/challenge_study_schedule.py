1491d219fdf317ede57e553b1442539f66ef0b01
Antonio Sabino <aslimajr@outlook.com>
2022-10-31 17:56:06 -0300

    Resolução requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
+from datetime import datetime
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if target_time is None:
+        return None
+    counter = 0
+    for time_in, time_out in permanence_period:
+        if (type(time_in) is str or time_in is None or time_out is None or type(time_out) is str):
+            return None
+        if time_in <= target_time <= time_out:
+            counter += 1
+    return counter
